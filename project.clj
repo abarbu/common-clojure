8d84665fe7df6ee85528484adc8c6a7f380233b1
Andrei Barbu <andrei@0xab.com>
2014-05-13 12:42:22 -0400

    sanity.core version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com._0xab/common-clojure "0.2.0"
+(defproject com._0xab/common-clojure "0.3.0"
   :description "Misc Clojure code shared among multiple projects"
   :url "https://github.com/abarbu/common-clojure"
   :license {:name "Eclipse Public License"
@@ -8,7 +8,7 @@
                  [opencv/opencv "2.4.7"]
                  [opencv/opencv-native "2.4.7"]
                  [com._0xab/nondeterminism "0.1.0"]
-                 [com._0xab/sanity "1.2.0"]
+                 [com._0xab/sanity "1.4.0"]
                  [commons-io "2.4"]
                  [incanter "1.5.4"]
                  [gloss "0.2.1"]
