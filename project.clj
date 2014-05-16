40ad11f4a2f954372107cd40b25cdd3d80f39985
Andrei Barbu <andrei@0xab.com>
2014-05-15 23:21:57 -0400

    split out platform-specific dependencies into common-clojure-platform

--- project.clj
+++ project.clj
@@ -1,18 +1,16 @@
-(defproject com._0xab/common-clojure "0.3.0"
+(defproject com._0xab/common-clojure "0.4.0"
   :description "Misc Clojure code shared among multiple projects"
   :url "https://github.com/abarbu/common-clojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [bwo/monads "0.2.0-SNAPSHOT"]
-                 [opencv/opencv "2.4.7"]
-                 [opencv/opencv-native "2.4.7"]
+                 [bwo/monads "0.2.0"]
                  [com._0xab/nondeterminism "0.1.0"]
                  [com._0xab/sanity "1.4.0"]
                  [commons-io "2.4"]
-                 [incanter "1.5.4"]
-                 [gloss "0.2.1"]
+                 [incanter "1.5.5"]
+                 [gloss "0.2.2"]
                  [org.jblas/jblas "1.2.3"]
-                 [com.taoensso/timbre "3.0.0-SNAPSHOT"]
+                 [com.taoensso/timbre "3.2.1"]
                  [com._0xab/clatrix "0.4.0-SNAPSHOT"]
                  [org.clojars.rosejn/clansi "1.2.0-SNAPSHOT"]])
